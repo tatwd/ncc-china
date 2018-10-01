3973ce952d43c6ae950b54a9717728c9fbe7d2f7
DM2N <sixnoodle@163.com>
2018-10-01 21:30:21 +0800

    feat(layouts): add layouts

---
+++ src/client/nuxt-web/layouts/account.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="layout-account">
+    <nuxt />
+    <canvas id="particle-animation"></canvas>
+  </div>
+</template>
+
+<script>
+import particleAnimation from '~/assets/js/particle-animation.js'
+
+export default {
+  name: 'account',
+  mounted () {
+    particleAnimation(document.querySelector('#particle-animation'))
+  }
+}
+</script>
+
+<style>
+canvas {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 0;
+  width: 100%;
+  height: 100%;
+  pointer-events: none;
+}
+</style>
---
+++ src/client/nuxt-web/layouts/error.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="layout-error">
+    <h1 v-if="error.statusCode === 404">页面不存在</h1>
+    <h1 v-else>应用发生错误异常</h1>
+    <nuxt-link to="/">首 页</nuxt-link>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'error',
+  props: ['error']
+}
+</script>
