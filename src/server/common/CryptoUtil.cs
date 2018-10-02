a0fbf0b20080c8dcb8a6d7c6f7802d19bfa0c373
tatwd <leoking9641@hotmail.com>
2018-10-02 23:17:44 +0800

    feat(common): add crypto util and encoding by salt

---
+++ src/server/common/CryptoUtil.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Ncc.China.Common
+{
+    public class CryptoUtil
+    {
+        public static string CreateSalt(int size)
+        {
+            using(var rng = new System.Security.Cryptography.RNGCryptoServiceProvider())
+            {
+                var buffer = new byte[size];
+                rng.GetBytes(buffer);
+                return System.Convert.ToBase64String(buffer);
+            }
+        }
+
+        public static string Encrypt(string password, string salt)
+        {
+            using (var sha256Managed = new System.Security.Cryptography.SHA256Managed())
+            {
+                var bytes = System.Text.Encoding.UTF8.GetBytes($"{password}{salt}");
+                var hashing = sha256Managed.ComputeHash(bytes);
+                return  ByteArrayToString(hashing);
+            }
+        }
+
+        public static string ByteArrayToString(byte[] bytes)
+        {
+            var result = new System.Text.StringBuilder();
+            foreach (var number in bytes)
+            {
+                result.Append(Hexadecimalize(number, false, false));
+            }
+            return result.ToString();
+        }
+
+        public static string Hexadecimalize(int number, bool hex = true, bool isUpper = true)
+        {
+            var result = new System.Text.StringBuilder();
+            if (hex)
+            {
+                result.AppendFormat("0X{0:X}", number);
+            }
+            else
+            {
+                result.AppendFormat("{0:X}", number);
+            }
+            if (!isUpper)
+            {
+                return result.ToString().ToLowerInvariant();
+            }
+            return result.ToString();
+        }
+    }
+}
