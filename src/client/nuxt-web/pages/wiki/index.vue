4bd55895402d7fecdbcf45cd7358de0d7b47ed2c
DM2N <sixnoodle@163.com>
2018-10-16 16:31:52 +0800

    feat(wiki): add wiki page

---
+++ src/client/nuxt-web/pages/wiki/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="wiki">
+    <el-card shadow="hover">
+      <p>本栏目致力于组织整理 .NET Core 中文社区的精华文章，便于大家快速查阅。</p>
+      <p>欢迎热心开发者参与维护社区 Wiki 资料，你可以做：</p>
+      <ol>
+        <li>翻译国外高质量的 .NET Core 技术文章</li>
+        <li>撰写完整的 .NET Core 技术教程，新手教程等</li>
+        <li>撰写或者翻译热门 .NET Core 的中文使用指导</li>
+        <li>分享各类部署方案</li>
+      </ol>
+      <h2>必读推荐</h2>
+      <hr>
+      <h3>.NET Core 入门教程</h3>
+      <h3>.NET Core 进阶教程</h3>
+      <h3>.NET Core 高级教程</h3>
+    </el-card>
+  </div>
+</template>
