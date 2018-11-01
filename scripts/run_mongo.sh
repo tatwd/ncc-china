5ed6f9a3a141a16858eaf2bda80533b7d00117a6
tatwd <leoking9641@hotmail.com>
2018-11-01 21:55:44 +0800

    chore: run db without `--rm`

--- scripts/run_mongo.sh
+++ scripts/run_mongo.sh
@@ -3,4 +3,4 @@ out_port=27017
 mongodb_img_tag="latest"
 
 echo "[Docker] Start running MongoDB on port $out_port ..."
-docker run --rm --name $container_name -p $out_port:27017 -d mongo:$mongodb_img_tag
+docker run --name $container_name -p $out_port:27017 -d mongo:$mongodb_img_tag
