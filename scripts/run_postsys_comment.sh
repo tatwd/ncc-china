1af5042c2eb7f8c8360dca6bd7b69cf4901cd120
tatwd <leoking9641@hotmail.com>
2019-05-15 14:13:11 +0800

    chore: mv old shell to scripts dir

--- scripts/run_postsys_comment.sh
+++ scripts/run_postsys_comment.sh
@@ -1,7 +1,7 @@
 echo "[Service] Start running postsys_comment api ..."
 
 cd src/server/services/postsys_comment
-if [ ! -d "node_modules"  ]; then
+if [ ! -d "node_modules" ]; then
   npm install
 fi
 npm run dev
