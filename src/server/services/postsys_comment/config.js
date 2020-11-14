19c452448aede950e604399d6d12efbfe23957a0
tatwd <leoking9641@hotmail.com>
2020-11-14 16:23:23 +0800

    chore: connect mongodb by auth

--- src/server/services/postsys_comment/config.js
+++ src/server/services/postsys_comment/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   secretKey: 'this_is_a_test_security_key',
-  nccPostsysDbConn: `mongodb://localhost:27017/ncc_postsys`
+  nccPostsysDbConn: `mongodb://root:test123@localhost:27017/ncc_postsys?authSource=admin`
 }
--- src/server/services/postsys_comment/config.prod.js
+++ src/server/services/postsys_comment/config.prod.js
@@ -1,4 +1,4 @@
 module.exports = {
   secretKey: 'this_is_a_test_security_key',
-  nccPostsysDbConn: `mongodb://ncc.postsys:27017/ncc_postsys`
+  nccPostsysDbConn: `mongodb://root:test123@ncc.postsys:27017/ncc_postsys?authSource=admin`
 }
